a27a58a59a37fbf0af24f1b64cf0a14f788b74bd
Scott Jones <scottdj92@gmail.com>
2018-11-17 11:40:37 -0500

    Initial scaffold + basic component addition

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Changing the title of this project"/>
+    <ListItem title="adding something new"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
 
 @Component({
   components: {
     HelloWorld,
+    Counter,
   },
 })
 export default class App extends Vue {}
@@ -19,7 +22,7 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Counter extends Vue {
+    @Prop() private title!: string;
+}
+</script>
+
+
+<style lang="scss" scoped>
+    h1 {
+        color: red;
+    }
+</style>
+
+
